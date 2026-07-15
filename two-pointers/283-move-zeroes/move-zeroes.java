61922885cbf89950f1f7629ffa6ba21075fe0068
adivisantoshi <155537623+adivisantoshi@users.noreply.github.com>
2026-07-15 07:19:38 +0530

    Time: 4 ms (10.04%) | Memory: 47.9 MB (20.33%) - LeetSync

---
+++ two-pointers/283-move-zeroes/move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+ int i=0,j=0;
+ int temp=0;
+ for(j=0;j<nums.length;j++){
+    if(nums[j]!=0){
+   temp=nums[i];
+    nums[i]=nums[j];
+    nums[j]=temp;
+    i++;
+    }}
+ System.out.println(nums);
+    }
+}
