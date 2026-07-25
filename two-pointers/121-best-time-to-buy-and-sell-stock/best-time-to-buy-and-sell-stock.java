6eca806005615735916eb2882e8d20a829292f36
adivisantoshi <155537623+adivisantoshi@users.noreply.github.com>
2026-07-26 05:18:30 +0530

    Time: 2 ms (42.29%) | Memory: 94.4 MB (71.20%) - LeetSync

--- two-pointers/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ two-pointers/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,18 +1,18 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int mip=prices[0];
-        int maxp=0,p=0;
-    
-    for(int i=1;i<prices.length;i++){
-        if(prices[i]<mip){
-            mip=prices[i];
-        }
-     p=prices[i]-mip;
-
-        if(p>maxp){
+   int minp=prices[0];
+   int maxp=0;
+   for(int i=1;i<prices.length;i++){
+    if(prices[i]<minp){
+        minp=prices[i];
+      
+    }
+    else{
+        int p=prices[i]-minp;
+        if(p>maxp)
         maxp=p;
-           }       }            
-        return maxp;
-        
-    
+    }
+      }  
+       
+    return maxp;
 }}
